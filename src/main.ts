33f7047eac66847644bb041887fb8f78c1c426a9
V1shvesh <22243137+V1shvesh@users.noreply.github.com>
2019-09-22 10:46:48 +0530

    Demo Huffman Codec

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
+import { readFile, writeFile } from "fs";
+import { join } from "path";
+
 import Huffman from "./huffman";
 
 const compressor = new Huffman();
 
-compressor.compress("assassin");
+// Compression
+const {compressedString, root} = compressor.compress("assassin");
+const JSONTree = JSON.stringify(root.toObject(), null, 2);
+process.stdout.write(`Compressed String: ${compressedString}\n`);
+// process.stdout.write(`Tree: ${JSONTree}`);
+
+writeFile(
+  join(__dirname, "../output/compressedString"),
+  compressedString,
+  (err) => err && process.stderr.write(`${err.message}\n`),
+);
+
+writeFile(
+  join(__dirname, "../output/tree.json"),
+  JSONTree,
+  (err) => err && process.stderr.write(`${err.message}\n`),
+);
+
+process.stdout.write(`Decompressed String: ${compressor.decompress(compressedString, root)}\n`);
